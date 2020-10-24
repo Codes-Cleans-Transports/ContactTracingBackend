20a8d4b3c7d238a9c52ca09bd4c1f09257d19d57
Radoslav Hubenov <rrhubenov@gmail.com>
2020-10-25 01:29:14 +0300

    create or update contacts

--- contact_tracing/contact/services.py
+++ contact_tracing/contact/services.py
@@ -1,15 +1,29 @@
 from datetime import datetime
 from user.models import User, ContactsRel
+from user.services import process_get_or_create_user
+
+
+
+def process_create_or_update_contacts(
+    *,
+    user,
+    macs
+) -> None:
+    for mac in macs:
+        target = process_get_or_create_user(mac=mac)
+        create_or_update_contact(user1=user, user2=targer)
 
 def create_or_update_contact(
     *,
-    mac1: str,
-    mac2: str,
+    user1: User,
+    user2: User
 ) -> ContactsRel:
-    user1 = User.nodes.get(mac=mac1)
-    user2 = User.nodes.get(mac=mac2)
-    start = datetime.today()
-    
-    contact = user1.contacts.connect(user2, {'start':start, 'duration':1})
 
-    return contact
+    if user1.contacts.is_created(user2):
+        relationship = user1.contacts.relationship(user2)
+        relationship.duration += 1
+        relationship.save()
+    else:
+        relationship = user1.contacts.connect(user2, {'duration': 1})
+    
+    return relationship
