d2589c2e6d1f8a7209b2fae33a7b444420de267e
Radoslav Hubenov <rrhubenov@gmail.com>
2020-10-25 01:50:31 +0300

    propagate safety on new contact

--- contact_tracing/contact/services.py
+++ contact_tracing/contact/services.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from user.models import User, ContactsRel
 from user.services import process_get_or_create_user
 
+from user.services import propagate_safety
+
 
 
 def process_create_or_update_contacts(
@@ -11,7 +13,7 @@ def process_create_or_update_contacts(
 ) -> None:
     for mac in macs:
         target = process_get_or_create_user(mac=mac)
-        create_or_update_contact(user1=user, user2=targer)
+        create_or_update_contact(user1=user, user2=target)
 
 def create_or_update_contact(
     *,
@@ -25,5 +27,10 @@ def create_or_update_contact(
         relationship.save()
     else:
         relationship = user1.contacts.connect(user2, {'duration': 1})
+
+    if user1.safety > user2.safety:
+        propagate_safety(user2)
+    else:
+        propagate_safety(user1)
     
     return relationship
