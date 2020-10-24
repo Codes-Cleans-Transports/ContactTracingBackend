002f948e158838b22ac5fb89d185ce91f82bdffa
Dany Yanev <dany.yanev@ocado.com>
2020-10-24 22:52:43 +0300

    working calcualte safety with tests

--- contact_tracing/contact/tests.py
+++ contact_tracing/contact/tests.py
@@ -8,6 +8,8 @@ from neomodel import db
 
 from user.services import *
 
+from random import randrange
+
 # Create your tests here.
 
 class ContactsTests(TestCase):
@@ -16,22 +18,60 @@ class ContactsTests(TestCase):
         db.cypher_query("MATCH (n) DETACH DELETE n;")
 
     def test_connect(self):
-
         date = datetime.today()
 
-        user1 = User(mac = "mac1", status = "Negative").save()
-        user2 = User(mac = "mac2", status = "Negative").save()
+        user1 = User(mac = "mac1", status = "negative").save()
+        user2 = User(mac = "mac2", status = "negative").save()
 
         user1.contacts.connect(user2, {'start': date, 'duration': 1})
 
     def test_mark_positive(self):
-        user1 = User(mac = "mac1", status = "Negative").save()
+        user1 = User(mac = "mac1", status = "negative").save()
 
         mark_positive(user1)
 
         user1 = get_user(mac = user1.mac)
-        self.assert_(user1.status == "Positive")
+        self.assert_(user1.status == "positive")
         self.assertAlmostEqual(user1.safety, 0)
+
+    def test_calc_occ_weight(self):
+        self.assertEqual(calc_occ_weight(0), 0)
+        self.assertEqual(calc_occ_weight(10), 0.05)
+        self.assertEqual(calc_occ_weight(90), 0.8)
+        self.assertEqual(calc_occ_weight(999990), 0.99)
+        
+        res = calc_occ_weight(randrange(1, 10))
+        self.assertTrue( 0 < res and res < 0.05)
+
+        res = calc_occ_weight(randrange(11, 89))
+        self.assertTrue( 0.05 < res and res < 0.8)
+
+        res = calc_occ_weight(randrange(90, 999))
+        self.assertTrue( 0.8 < res and res < 1)
+
+    def test_calculate_safety(self):
+        occ = 10
+        safety = 1
+        vertex = ContactsRel(start=datetime.today(), duration = occ)
+        user = User(mac="mac1", status = "negative", safety = safety)
+
+        self.assertEqual(calculate_safety(user, vertex), 1.0)
+
+        occ = 10
+        safety = 0
+        vertex = ContactsRel(start=datetime.today(), duration = occ)
+        user = User(mac="mac1", status = "negative", safety = safety)
+
+        self.assertEqual(calculate_safety(user, vertex), 0.95)
+
+        occ = 90
+        safety = .5
+        vertex = ContactsRel(start=datetime.today(), duration = occ)
+        user = User(mac="mac1", status = "negative", safety = safety)
+
+        self.assertEqual(calculate_safety(user, vertex), 0.6)
+
+
 
 
 
--- contact_tracing/user/services.py
+++ contact_tracing/user/services.py
@@ -27,7 +27,29 @@ def create_user(
 def mark_positive(user: User):
     user1 = get_user(mac = user.mac)
 
-    user1.status = "Positive"
+    user1.status = "positive"
     user1.safety = 0
 
     user1.save()
+
+def calc_occ_weight(occ: int) -> float:
+    res = -1
+    if occ <= 10:
+	    res = 0.5 * occ
+    elif occ <= 90:
+	    res = 0.8823529411764706 * occ + 0.5882352941176521
+    elif occ < 1000:
+	    res = 0.020879120879120878 * occ + 78.12087912087912
+    else:
+	    res = 99
+
+    return res / 100
+
+
+def calculate_safety(incoming_user: User, vertex: ContactsRel) -> float:
+    occurenses_weight = calc_occ_weight(vertex.duration)
+
+    user_risk = 1 - incoming_user.safety
+
+    result = occurenses_weight * user_risk
+    return 1 - result
