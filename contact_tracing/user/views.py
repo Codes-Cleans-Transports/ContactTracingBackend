c2222bb5a8343d7418262e858caae8f62b211297
Radoslav Hubenov <rrhubenov@gmail.com>
2020-10-24 21:26:57 +0300

    remove user creat view

--- contact_tracing/user/views.py
+++ contact_tracing/user/views.py
@@ -3,31 +3,9 @@ from rest_framework import serializers, views
 from rest_framework.response import Response
 from rest_framework import status
 
-from .services import create_user
-from .selectors import get_user
+from .services import process_get_or_create_user
 
 # Create your views here.
-
-
-class UserCreateView(views.APIView):
-
-    class InputSerializer(serializers.Serializer):
-        mac = serializers.CharField(required=True)
-
-    class OutputSerializer(serializers.Serializer):
-        id = serializers.CharField()
-
-    def post(self, request):
-        
-        serializer = self.InputSerializer(data=request.data)
-
-        serializer.is_valid(raise_exception=True)
-
-        user = create_user(**serializer.validated_data)
-
-        output_serializer = self.OutputSerializer({'id': user.id})
-
-        return Response(data=output_serializer.data)
 
 
 class UserDetailsView(views.APIView):
@@ -37,7 +15,7 @@ class UserDetailsView(views.APIView):
     
     def get(self, request, mac):
         
-        user = get_user(mac=mac)
+        user = process_get_or_create_user(mac=mac)
 
         output_serializer = self.OutputSerializer({'status': user.status})
 
