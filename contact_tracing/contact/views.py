258773d5a78c43e4f9cbd9026fa6dc21a58f9f0e
Radoslav Hubenov <rrhubenov@gmail.com>
2020-10-25 01:30:02 +0300

    general view for contacts

--- contact_tracing/contact/views.py
+++ contact_tracing/contact/views.py
@@ -2,47 +2,58 @@ from django.shortcuts import render
 from rest_framework import serializers, views, status
 from rest_framework.response import Response
 from rest_framework.serializers import IntegerField, DateTimeField, DictField, ListField
-from .services import create_or_update_contact
+from .services import process_create_or_update_contact
 from user.models import User, ContactsRel
 from user.selectors import get_user
 
 
 # Create your views here.
-class ContactCreateView(views.APIView):
-
-    class InputSerializer(serializers.Serializer):
-        mac = serializers.CharField(required=True)
-
-    class OutputSerializer(serializers.Serializer):
-        pass
-
-    def post(self, request, mac):
-        serializer = self.InputSerializer(data=request.data)
-
-        serializer.is_valid(raise_exception=True)
-
-        contact = create_or_update_contact(**serializer.validated_data)
-
-        return Response(status=status.HTTP_201_CREATED)
-
-
-class ContactDetailView(views.APIView):
+class ContactCreateDetailView(views.APIView):
 
     class OutputSerializer(serializers.Serializer):
         contacts = DictField()
         start = ListField()
         duration = ListField()
 
+    class InputSerializer(serializers.Serializer):
+        macs = serializers.ListField(child=serializers.CharField())
+
+    def post(self, request, mac):
+        serializer = self.InputSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        user = get_user(mac=mac)
+
+        contact = process_create_or_update_contacts(user=user, **serializer.validated_data)
+
+        return Response(status=status.HTTP_201_CREATED)
+
     def get(self, request, mac):
         user = get_user(mac=mac)
         start = []
         duration = []
 
-        #import pdb; pdb.set_trace()
         for contact in user.contacts.all():
             start.append(user.contacts.relationship(contact).start)
             duration.append(user.contacts.relationship(contact).duration)
 
+        serializer = self.OutputSerializer({'contacts': user.contacts.all(), 'start': start, 'duration': duration})
+
+        return Response(data=serializer.data)
+
+class ContactDetailView(views.APIView):
+
+   
+
+    def get(self, request, mac):
+        user = get_user(mac=mac)
+        start = []
+        duration = []
+
+        for contact in user.contacts.all():
+            start.append(user.contacts.relationship(contact).start)
+            duration.append(user.contacts.relationship(contact).duration)
 
         serializer = self.OutputSerializer({'contacts': user.contacts.all(), 'start': start, 'duration': duration})
 
